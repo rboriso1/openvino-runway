ad8deaf90b2575aec7bbdfe52b34391f475cf422
rboriso1 <43966741+rboriso1@users.noreply.github.com>
2019-10-24 11:05:10 -0700

    Added classification squeezenet model.
    Added classification sample based on squeezenet model.

---
+++ models/classification/runway_model.py
@@ -0,0 +1,109 @@
+# MIT License
+
+# Copyright (c) 2019 Runway AI, Inc
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# =========================================================================
+
+# This example contains the minimum specifications and requirements
+# to port a machine learning model to Runway.
+
+# For more instructions on how to port a model to Runway, see the Runway Model
+# SDK docs at https://sdk.runwayml.com
+
+# RUNWAY
+# www.runwayml.com
+# hello@runwayml.com
+
+# =========================================================================
+
+# Import the Runway SDK. Please install it first with
+# `pip install runway-python`.
+import runway
+from runway.data_types import number, text, image
+from example_model import ExampleModel
+from argparse import ArgumentParser, SUPPRESS
+
+# Setup the model, initialize weights, set the configs of the model, etc.
+# Every model will have a different set of configurations and requirements.
+# Check https://docs.runwayapp.ai/#/python-sdk to see a complete list of
+# supported configs. The setup function should return the model ready to be
+# used.
+#setup_options = {
+#    'truncation': number(min=1, max=10, step=1, default=5, description='Example input.'),
+#    'seed': number(min=0, max=1000000, description='A seed used to initialize the model.')
+#}
+
+def build_argparser():
+    parser = ArgumentParser(add_help=False)
+    args = parser.add_argument_group('Options')
+    args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
+    args.add_argument("-m", "--model", help="Required. Path to an .xml file with a trained model.", required=True,
+                      type=str)
+#    args.add_argument("-i", "--input", help="Required. Path to a folder with images or path to an image files",
+#                      required=True,
+#                      type=str, nargs="+")
+    args.add_argument("-l", "--cpu_extension",
+                      help="Optional. Required for CPU custom layers. "
+                           "MKLDNN (CPU)-targeted custom layers. Absolute path to a shared library with the"
+                           " kernels implementations.", type=str, default=None)
+    args.add_argument("-d", "--device",
+                      help="Optional. Specify the target device to infer on; CPU, GPU, FPGA, HDDL, MYRIAD or HETERO: is "
+                           "acceptable. The sample will look for a suitable plugin for device specified. Default "
+                           "value is CPU",
+                      default="CPU", type=str)
+    args.add_argument("--labels", help="Optional. Path to a labels mapping file", default=None, type=str)
+    args.add_argument("-nt", "--number_top", help="Optional. Number of top results", default=10, type=int)
+
+    return parser
+
+@runway.setup
+def setup():
+    #print(msg.format(opts['seed'], opts['truncation']))
+    args = build_argparser().parse_args()
+    model = ExampleModel(args)
+    return model
+
+# Every model needs to have at least one command. Every command allows to send
+# inputs and process outputs. To see a complete list of supported inputs and
+# outputs data types: https://sdk.runwayml.com/en/latest/data_types.html
+@runway.command(name='classify',
+                inputs={ 'photo': image() },
+                outputs={ 'label': text() },
+                description='classifies image')
+def classify(model, args):
+    #print('[GENERATE] Ran with caption value "{}"'.format(args['caption']))
+    # Generate a PIL or Numpy image based on the input caption, and return it
+    label = model.run_on_input(args['photo'])
+    return {
+        'label': label
+    }
+
+if __name__ == '__main__':
+    # run the model server using the default network interface and ports,
+    # displayed here for convenience
+    runway.run(host='0.0.0.0', port=9000)
+
+## Now that the model is running, open a new terminal and give it a command to
+## generate an image. It will respond with a base64 encoded URI
+# curl \
+#   -H "content-type: application/json" \
+#   -d '{ "caption": "red" }' \
+#   localhost:8000/generate
