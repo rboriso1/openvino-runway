4036bc69a347b134d9db5556b09226240e0e35ac
rboriso1 <43966741+rboriso1@users.noreply.github.com>
2019-11-01 11:12:50 -0700

    Added missing model_options parameter

--- runway_model.py
+++ runway_model.py
@@ -103,7 +103,7 @@ def classify(model, args):
 if __name__ == '__main__':
     # run the model server using the default network interface and ports,
     # displayed here for convenience
-    runway.run(host='0.0.0.0', port=9000)
+    runway.run(host='0.0.0.0', port=9000, model_options={'model_directory': './models/squeezenet/1'})
 
 ## Now that the model is running, open a new terminal and give it a command to
 ## generate an image. It will respond with a base64 encoded URI
